f8c6d72994bcf3e75e15b8275be8bb61f6fd984f
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-07-13 22:51:38 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,28 @@
+class Genre
+  attr_accessor :name
+  attr_reader :songs
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    all.clear
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.create(name)
+    genre = new(name)
+    genre.save
+    genre
+  end
+end
