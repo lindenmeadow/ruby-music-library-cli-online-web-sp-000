3b49fe029fda2192599e52a338bf75618a891909
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-07-13 22:45:57 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,18 @@
+class Artist
+  attr_accessor :name, :songs
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
+  
+end
