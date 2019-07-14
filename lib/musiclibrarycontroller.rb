faeca19adf2c8b7f07b101e9dfed8e173b2df148
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-07-14 02:34:29 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -20,4 +20,8 @@ class MusicLibraryController
     end
   end
 
+  def list_songs
+    Song.all.sort{|a,b| a.name <=> b.name.}.each_with_index{|song, index| puts "#{index + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
+  end
+
 end
