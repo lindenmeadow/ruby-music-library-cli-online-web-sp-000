73e1435ec7050b0272e74bc1472f6766c5dffb34
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-07-14 03:16:30 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -17,6 +17,16 @@ class MusicLibraryController
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
       input = gets.strip
+
+      case input
+      when "list songs"
+        list_songs
+      when "list artists"
+        list_artists
+      when "list genres"
+        list_genres
+      when "list artist"
+        list_artists
     end
   end
 
